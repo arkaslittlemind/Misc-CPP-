ab43891068e1290f2937d9fda186dd2d793a7468
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-04 18:49:05 +0530

    a

---
+++ C++ STL/pairs&vectors.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    pair<int, string> p;
+    p = make_pair(2, "abc");
+
+    cout << p.first << " " << p.second << endl;
+    
+}
