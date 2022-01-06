284c03912b6a77995279f8e224d5c656f7d6b5d2
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-06 19:54:54 +0530

    1

--- C++ STL/pairs&vectors.cpp
+++ C++ STL/pairs&vectors.cpp
@@ -1,12 +1,11 @@
 #include<bits/stdc++.h>
-#include<vector>
+
 using namespace std;
 
 int main(){
 
-    pair<int, string> p;
-    p = make_pair(2, "abc");
+    cout << 1 << endl;
 
-    cout << p.first << " " << p.second << endl;
+    return 0;
     
 }
