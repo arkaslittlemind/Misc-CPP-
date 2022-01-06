dd7f5acc149876a3b4aacc91a350e3ec7cf72d27
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-06 21:34:56 +0530

    Add files via upload

---
+++ c++11-iterators.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	vector<pair<int, int>> vec_p = {{1, 2}, {2, 3}};
+	for (auto &value : vec_p) {
+		cout << value.first << " " << value.second << endl;
+	}
+	auto a = 1.1;
+	cout << a << endl;
+}
